1c5fc801279d4f78ef6ceb9f47bca99b4718f61a
sun5hine <af5kh@yahoo.com>
2018-10-20 19:33:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+ prime_number = [1, 2, 5, 7]
+if prime_number.include?(number)
+puts true
+elsif
+prime_number.each {|x| number % x == 0 }
+puts false
+
+end
+end
